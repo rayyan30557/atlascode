9dc7cf9280b20899ba7377605c28fd6963faf8b7
Christian Abella <cabella@atlassian.com>
2025-04-30 15:39:16 -1000

    AXON-112: add rte hook

---
+++ src/webviews/components/issue/common/Editor.tsx
@@ -0,0 +1,265 @@
+import { baseKeymap } from 'prosemirror-commands';
+import { dropCursor } from 'prosemirror-dropcursor';
+import { buildKeymap, buildMenuItems } from 'prosemirror-example-setup';
+import { gapCursor } from 'prosemirror-gapcursor';
+import { history } from 'prosemirror-history';
+import { InputRule, inputRules, textblockTypeInputRule, wrappingInputRule } from 'prosemirror-inputrules';
+import { keymap } from 'prosemirror-keymap';
+import {
+    defaultMarkdownParser,
+    defaultMarkdownSerializer,
+    MarkdownParser,
+    MarkdownSerializer,
+    schema as markdownSchema,
+} from 'prosemirror-markdown';
+import { addMentionNodes, getMentionsPlugin } from 'prosemirror-mentions';
+import { menuBar } from 'prosemirror-menu';
+import { Schema } from 'prosemirror-model';
+import { EditorState, Plugin, PluginKey } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+function markInputRule(regexp: RegExp, markType: any, getAttrs: any) {
+    return new InputRule(regexp, (state: EditorState, match: string[], start: number, end: number) => {
+        const attrs = getAttrs instanceof Function ? getAttrs(match) : getAttrs;
+        const tr = state.tr;
+        const completeMatch = match[0];
+        const prefix = match.length > 2 ? match[1] : '';
+        const text = match.length > 2 ? match[2] : match[1];
+        if (text) {
+            const textStart = start + completeMatch.indexOf(text);
+            const textEnd = textStart + text.length;
+            if (textEnd < end) {
+                tr.delete(textEnd, end);
+            }
+            if (textStart > start) {
+                tr.delete(start + prefix.length, textStart);
+            }
+            end = start + prefix.length + text.length;
+        }
+
+        tr.addMark(start + prefix.length, end, markType.create(attrs));
+        tr.removeStoredMark(markType); // Do not continue with mark.
+        return tr;
+    });
+}
+
+const buildInputRules = (schema: any) => {
+    const rules = [];
+
+    if (schema.marks.strong) {
+        rules.push(markInputRule(/(?:\*\*)([^\*_]+)(?:\*\*|__)$/, schema.marks.strong, undefined));
+        rules.push(markInputRule(/(?:__)([^_]+)(?:__)$/, schema.marks.strong, undefined));
+    }
+    if (schema.marks.em) {
+        rules.push(markInputRule(/(^|[^\*])(?:\*)([^\*]+)(?:\*)$/, schema.marks.em, undefined));
+        rules.push(markInputRule(/(^|[^_])(?:_)([^_]+)(?:_)$/, schema.marks.em, undefined));
+    }
+    if (schema.marks.link) {
+        rules.push(
+            markInputRule(/(^|[^!])\[(.*?)\]\((\S+)\)$/, schema.marks.link, (match: string[]) => ({ href: match[3] })),
+        );
+    }
+
+    rules.push(
+        wrappingInputRule(/^\s*>\s$/, schema.nodes.blockquote),
+        wrappingInputRule(
+            /^(\d+)\.\s$/,
+            schema.nodes.ordered_list,
+            (match: string[]) => ({ order: +match[1] }),
+            (match: string[], node: any) => node.childCount + node.attrs.order === +match[1],
+        ),
+        wrappingInputRule(/^\s*([-+*])\s$/, schema.nodes.bullet_list),
+        textblockTypeInputRule(/^```$/, schema.nodes.code_block),
+        textblockTypeInputRule(new RegExp('^(#{1,6})\\s$'), schema.nodes.heading, (match: string[]) => ({
+            level: match[1].length,
+        })),
+    );
+    return rules;
+};
+
+const schema = new Schema({
+    nodes: addMentionNodes(markdownSchema.spec.nodes),
+    marks: markdownSchema.spec.marks,
+});
+
+/**
+ * This is to find all mentions in the text node of the editor to be able to remove escape characters
+ * @param str The string to search
+ */
+function findAllMentions(str: string) {
+    const regex = /\[~[a-zA-Z0-9:-]+\]/;
+    const matches = str.match(regex);
+    return matches;
+}
+
+// https://github.com/ProseMirror/prosemirror-markdown/blob/master/src/to_markdown.js
+const mdSerializer = new MarkdownSerializer(
+    {
+        ...defaultMarkdownSerializer.nodes,
+        mention: (state: any, node: any) => {
+            state.write(node.attrs.id);
+        },
+        text: (state: any, node: any) => {
+            const matches = findAllMentions(node.text);
+            let text: string = node.text;
+            if (matches && matches.length > 0) {
+                for (const match of matches) {
+                    const tmp = text.split(match);
+
+                    state.text(tmp[0], !state.isAutolink);
+                    state.write(match);
+                    text = tmp[1];
+                }
+            }
+
+            state.text(text, !state.isAutolink);
+        },
+    },
+    defaultMarkdownSerializer.marks,
+);
+
+const mdParser = new MarkdownParser(schema, defaultMarkdownParser.tokenizer, {
+    ...defaultMarkdownParser.tokens,
+    mention: {
+        node: 'mention',
+    },
+});
+
+/**
+ * IMPORTANT: outer div's "suggestion-item-list" class is mandatory. The plugin uses this class for querying.
+ * IMPORTANT: inner div's "suggestion-item" class is mandatory too for the same reasons
+ */
+const getMentionSuggestionsHTML = (items: any[]) =>
+    '<div class="suggestion-item-list">' +
+    items.map((i) => `<div class="suggestion-item">${i.name}</div>`).join('') +
+    '</div>';
+
+const reactPropsKey = new PluginKey('reactProps');
+
+function reactProps(initialProps: any) {
+    return new Plugin({
+        key: reactPropsKey,
+        state: {
+            init: () => initialProps,
+            apply: (tr, prev) => tr.getMeta(reactPropsKey) || prev,
+        },
+    });
+}
+
+interface UserType {
+    displayName: string;
+    mention: string;
+    emailAddress?: string;
+    avatarUrl?: string;
+}
+
+export function useEditor<T extends UserType>(props: {
+    value: string;
+    enabled: boolean;
+    onSave?: (input: string) => void;
+    fetchUsers?: (input: string) => Promise<T[]>;
+}) {
+    const viewHost = useRef<HTMLDivElement>(null);
+    const view = useRef<EditorView | null>(null);
+    const [content, setContent] = useState(props.value || '');
+
+    const handleSave = useCallback(() => {
+        const mdContent: string = props.enabled ? mdSerializer.serialize(view.current!.state.doc) : content;
+        console.log(mdContent);
+        if (mdContent.trim().length > 0) {
+            try {
+                props.onSave?.(mdContent);
+            } catch {
+                return;
+            }
+        }
+    }, [props, content]);
+
+    useEffect(() => {
+        if (!view.current) {
+            return;
+        }
+        if (props.enabled) {
+            const slice = view.current.state.doc.slice(0);
+            const tr = view.current.state.tr.replaceWith(0, slice?.size || 0, mdParser.parse(content));
+            view.current.dispatch(tr);
+        } else {
+            setContent(mdSerializer.serialize(view.current.state.doc));
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.enabled]);
+
+    useEffect(() => {
+        // initial render
+        const menuItems = buildMenuItems(schema);
+        const plugins = [
+            reactProps(props),
+            keymap(buildKeymap(schema)),
+            keymap(baseKeymap),
+            dropCursor(),
+            gapCursor(),
+            menuBar({
+                floating: false,
+                content: [
+                    [menuItems.toggleStrong, menuItems.toggleEm, menuItems.toggleCode],
+                    [menuItems.wrapBulletList, menuItems.wrapOrderedList, menuItems.wrapBlockQuote],
+                ],
+            }),
+            history(),
+            inputRules({ rules: buildInputRules(schema) }),
+        ];
+
+        if (props.fetchUsers) {
+            plugins.unshift(
+                // https://github.com/joelewis/prosemirror-mentions
+                getMentionsPlugin({
+                    getSuggestions: async (type: any, text: string, done: any) => {
+                        if (type === 'mention') {
+                            const users = await props.fetchUsers!(text);
+                            done(
+                                users.map((u) => ({
+                                    name: u.displayName,
+                                    id: u.mention,
+                                    avatarUrl: u.avatarUrl || '',
+                                    email: u.emailAddress || '',
+                                })),
+                            );
+                        }
+                    },
+                    getSuggestionsHTML: (items: T[], type: any) => {
+                        if (type === 'mention') {
+                            return getMentionSuggestionsHTML(items);
+                        } else {
+                            return <></>;
+                        }
+                    },
+                    maxNoOfSuggestions: 5,
+                }),
+            );
+        }
+
+        const state = EditorState.create({
+            schema,
+            doc: mdParser.parse(content),
+            plugins: plugins,
+        });
+        console.log(content);
+        const currView = new EditorView(viewHost.current!, {
+            state,
+        });
+        view.current = currView;
+        return () => currView.destroy();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        // every render
+        if (view.current) {
+            const tr = view.current.state.tr.setMeta(reactPropsKey, props);
+            view.current.dispatch(tr);
+        }
+    });
+
+    return { viewHost, handleSave };
+}
--- src/webviews/components/issue/common/JiraIssueTextArea.tsx
+++ src/webviews/components/issue/common/JiraIssueTextArea.tsx
@@ -1,17 +1,19 @@
 import { Spacing } from '@atlaskit/button';
 import { ButtonAppearance } from '@atlaskit/button/dist/types/new-button/variants/types';
 import TextArea from '@atlaskit/textarea';
-import { Box } from '@material-ui/core';
+import Toggle from '@atlaskit/toggle';
+import Tooltip from '@atlaskit/tooltip';
 import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
 import React from 'react';
 
 import PopoutMentionPicker from '../../pullrequest/PopoutMentionPicker';
+import { useEditor } from './Editor';
 
 type Props = {
     value: string;
     onChange: (input: string) => void;
     onEditorFocus?: (e: any) => void;
-    onSave?: () => void;
+    onSave?: (i: string) => void;
     onCancel?: () => void;
     fetchUsers?: (input: string) => Promise<{ displayName: string; mention: string; avatarUrl?: string }[]>;
     isServiceDeskProject?: boolean;
@@ -19,6 +21,12 @@ type Props = {
     isDescription?: boolean;
     saving?: boolean;
 };
+
+interface User {
+    displayName: string;
+    mention: string;
+    avatarUrl?: string;
+}
 const JiraIssueTextAreaEditor: React.FC<Props> = ({
     value,
     onChange,
@@ -37,10 +45,19 @@ const JiraIssueTextAreaEditor: React.FC<Props> = ({
         spacing: 'compact' as Spacing,
         appearance: 'subtle' as ButtonAppearance,
     };
-    const [isPopoutOpen, setIsPopoutOpen] = React.useState(false);
-    const [isInline, setIsInline] = React.useState(false);
-    const [searchInput, setSearchInput] = React.useState('');
-    const [tmpValue, setTmpValue] = React.useState('');
+
+    const [rteEnabled, setRteEnabled] = React.useState(true);
+
+    const { viewHost, handleSave } = useEditor<User>({
+        value,
+        onSave: (i: string) => {
+            onChange(i);
+            onSave?.(i);
+        },
+        enabled: rteEnabled,
+        fetchUsers,
+    });
+
     React.useEffect(() => {
         if (inputTextAreaRef.current && cursorPosition > 0) {
             inputTextAreaRef.current.selectionEnd = cursorPosition;
@@ -49,70 +66,49 @@ const JiraIssueTextAreaEditor: React.FC<Props> = ({
         }
     }, [inputTextAreaRef, cursorPosition]);
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === '@') {
-            e.preventDefault();
-            setIsPopoutOpen(true);
-            setIsInline(true);
-            setTmpValue(value);
-        }
-    };
-
-    const handleSearchInputChange = (input: string) => {
-        if (!inputTextAreaRef.current) {
-            return;
-        }
-        const { selectionStart, selectionEnd, value } = inputTextAreaRef.current;
-        const val = tmpValue !== '' ? tmpValue : value;
-        const commentInputWithInline = `${val.slice(0, selectionStart)}${input} ${val.slice(selectionEnd)}`;
-        setCursorPosition(selectionStart + input.length);
-        setSearchInput(input);
-        onChange(commentInputWithInline);
-    };
     const handleMention = React.useCallback(
         (user: any) => {
             if (!inputTextAreaRef.current) {
                 return;
             }
-
             const { selectionStart, selectionEnd, value } = inputTextAreaRef.current;
-            const val = tmpValue !== '' ? tmpValue : value;
             const mentionText: string = user.mention;
-            const commentInputWithMention = `${val.slice(0, selectionStart)}${mentionText} ${val.slice(selectionEnd)}`;
+            const commentInputWithMention = `${value.slice(0, selectionStart)}${mentionText} ${value.slice(selectionEnd)}`;
             setCursorPosition(selectionStart + mentionText.length);
             onChange(commentInputWithMention);
-            setIsPopoutOpen(false);
         },
-        [onChange, tmpValue, inputTextAreaRef, setCursorPosition],
+        [onChange],
     );
     return (
-        <Box style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-            <TextArea
-                style={{
-                    background: 'var(--vscode-input-background)',
-                    color: 'var(--vscode-input-foreground)',
-                    border: '1px solid var(--vscode-input-border)',
-                    caretColor: 'var(--vscode-editorCursor-background)',
-                    minHeight: isDescription ? '175px' : '100px',
-                    borderRadius: '2px',
-                    overflow: 'auto',
-                }}
-                value={value}
-                ref={inputTextAreaRef}
-                autoFocus
-                onFocus={onEditorFocus ? onEditorFocus : undefined}
-                onChange={(e) => onChange(e.target.value)}
-                isDisabled={saving}
-                onKeyDown={handleKeyDown}
-            />
-            <Box
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <div hidden={!rteEnabled} ref={viewHost} />
+            <div hidden={rteEnabled}>
+                <TextArea
+                    style={{
+                        background: 'var(--vscode-input-background)',
+                        color: 'var(--vscode-input-foreground)',
+                        border: '1px solid var(--vscode-input-border)',
+                        caretColor: 'var(--vscode-editorCursor-background)',
+                        minHeight: isDescription ? '175px' : '100px',
+                        borderRadius: '2px',
+                        overflow: 'auto',
+                    }}
+                    value={value}
+                    ref={inputTextAreaRef}
+                    autoFocus
+                    onFocus={onEditorFocus ? onEditorFocus : undefined}
+                    onChange={(e) => onChange(e.target.value)}
+                    isDisabled={saving}
+                />
+            </div>
+            <div
                 style={{
                     display: 'flex',
                     justifyContent: 'space-between',
                     paddingTop: '8px',
                 }}
             >
-                <Box
+                <div
                     style={{
                         display: 'flex',
                         flexDirection: 'row',
@@ -121,7 +117,13 @@ const JiraIssueTextAreaEditor: React.FC<Props> = ({
                     }}
                 >
                     {onSave && (
-                        <VSCodeButton appearance="primary" onClick={onSave} disabled={saving}>
+                        <VSCodeButton
+                            appearance="primary"
+                            onClick={() => {
+                                rteEnabled ? handleSave() : onSave(value);
+                            }}
+                            disabled={saving}
+                        >
                             {isServiceDeskProject ? 'Reply' : 'Save'}
                         </VSCodeButton>
                     )}
@@ -130,35 +132,26 @@ const JiraIssueTextAreaEditor: React.FC<Props> = ({
                             Add internal note
                         </VSCodeButton>
                     )}
-                    {fetchUsers && (
+                    {onCancel && (
+                        <VSCodeButton appearance="secondary" onClick={onCancel} disabled={saving}>
+                            Cancel
+                        </VSCodeButton>
+                    )}
+                    {fetchUsers && !rteEnabled && (
                         <PopoutMentionPicker
-                            isInline={isInline}
-                            parentOnChange={handleSearchInputChange}
                             targetButtonContent="@"
                             targetButtonTooltip="Mention @"
-                            injectToggleOpen={() => {
-                                setIsPopoutOpen((prev) => !prev);
-                                if (isInline) {
-                                    setIsInline(false);
-                                    inputTextAreaRef.current?.focus();
-                                    handleSearchInputChange(searchInput);
-                                    setSearchInput('');
-                                }
-                            }}
-                            injectIsOpen={isPopoutOpen}
                             targetButtonProps={buttonProps}
                             loadUserOptions={fetchUsers}
                             onUserMentioned={handleMention}
                         />
                     )}
-                </Box>
-                {onCancel && (
-                    <VSCodeButton appearance="secondary" onClick={onCancel} disabled={saving}>
-                        Cancel
-                    </VSCodeButton>
-                )}
-            </Box>
-        </Box>
+                </div>
+                <Tooltip content="Toggle rich text editor" position="top">
+                    <Toggle defaultChecked onChange={(e) => setRteEnabled(e.target.checked)} />
+                </Tooltip>
+            </div>
+        </div>
     );
 };
 
